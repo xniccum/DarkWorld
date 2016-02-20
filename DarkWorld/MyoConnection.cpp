3e87d2a0057e23bf53a1ec87ecb76ebb179513d8
asleeponduty <lukedrong@gmail.com>
2016-02-20 09:10:35 -0600

    Added myo test code. Requires myo server to be running and runs instead of gunnars code (testing... :P)

---
+++ DarkWorld/MyoConnection.cpp
@@ -0,0 +1,117 @@
+#include "stdafx.h"
+#include <myo/myo.hpp>
+
+class MyoConnection : public myo::DeviceListener {
+public:
+	MyoConnection()
+		: onArm(false), currentPose()
+	{
+		
+	}
+	// onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
+	void onUnpair(myo::Myo* myo, uint64_t timestamp)
+	{
+		onArm = false;
+	}
+	// onPose() is called whenever the Myo detects that the person wearing it has changed their pose, for example,
+	// making a fist, or not making a fist anymore.
+	void onPose(myo::Myo* myo, uint64_t timestamp, myo::Pose pose)
+	{
+		currentPose = pose;
+
+		if (pose != myo::Pose::unknown && pose != myo::Pose::rest) {
+			// Tell the Myo to stay unlocked until told otherwise. We do that here so you can hold the poses without the
+			// Myo becoming locked.
+			//myo->unlock(myo::Myo::unlockHold);
+
+			// Notify the Myo that the pose has resulted in an action, in this case changing
+			// the text on the screen. The Myo will vibrate.
+			myo->notifyUserAction();
+			lastActivePose = currentPose;
+		}
+		else {
+			// Tell the Myo to stay unlocked only for a short period. This allows the Myo to stay unlocked while poses
+			// are being performed, but lock after inactivity.
+			//myo->unlock(myo::Myo::unlockTimed);
+		}
+	}
+	// onArmSync() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
+	// arm. This lets Myo know which arm it's on and which way it's facing.
+	void onArmSync(myo::Myo* myo, uint64_t timestamp, myo::Arm arm, myo::XDirection xDirection, float rotation,
+		myo::WarmupState warmupState)
+	{
+		onArm = true;
+		whichArm = arm;
+	}
+
+	// onArmUnsync() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+	// it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+	// when Myo is moved around on the arm.
+	void onArmUnsync(myo::Myo* myo, uint64_t timestamp)
+	{
+		onArm = false;
+	}
+	// These values are set by onArmSync() and onArmUnsync() above.
+	bool onArm;
+	myo::Arm whichArm;
+	
+	// These values are set  onPose() above.
+
+	myo::Pose currentPose = myo::Pose::rest;
+	myo::Pose lastActivePose = myo::Pose::rest;
+};
+//Casually commenting this out, leaving it here until we can implement. 
+int main(int argc, char** argv)
+{
+	// We catch any exceptions that might occur below -- see the catch statement for more details.
+	try {
+
+		// First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
+		// publishing your application. The Hub provides access to one or more Myos.
+		myo::Hub hub("com.darkworld.darkworld");
+		hub.setLockingPolicy(myo::Hub::lockingPolicyNone);
+
+		std::cout << "Attempting to find a Myo..." << std::endl;
+
+		// Next, we attempt to find a Myo to use. If a Myo is already paired in Myo Connect, this will return that Myo
+		// immediately.
+		// waitForMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
+		// if that fails, the function will return a null pointer.
+		myo::Myo* myo = hub.waitForMyo(10000);
+
+		// If waitForMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+		if (!myo) {
+			throw std::runtime_error("Unable to find a Myo!");
+		}
+
+		// We've found a Myo.
+		std::cout << "Connected to a Myo armband!" << std::endl << std::endl;
+
+		// Next we construct an instance of our DeviceListener, so that we can register it with the Hub.
+		MyoConnection collector;
+
+		// Hub::addListener() takes the address of any object whose class inherits from DeviceListener, and will cause
+		// Hub::run() to send events to all registered device listeners.
+		hub.addListener(&collector);
+
+		// Finally we enter our main loop.
+		while (1) {
+			// In each iteration of our main loop, we run the Myo event loop for a set number of milliseconds.
+			// In this case, we wish to update our display 20 times a second, so we run for 1000/20 milliseconds.
+			hub.run(1000 / 20);
+			
+			if (collector.currentPose != myo::Pose::rest){
+				std::cout << collector.lastActivePose << " - " << collector.currentPose << std::endl << std::endl;
+			}
+		}
+
+
+		// If a standard exception occurred, we print out its message and exit.
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		std::cerr << "Press enter to continue.";
+		std::cin.ignore();
+		return 1;
+	}
+}
--- DarkWorld/stdafx.h
+++ DarkWorld/stdafx.h
@@ -12,4 +12,6 @@
 
 
 
+
 // TODO: reference additional headers your program requires here
+#include <myo/myo.hpp>
